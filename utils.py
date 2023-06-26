86f3075a3c8f89c8fa0d392151a76395b7613800
Özgür Özkan <ozgurozkan123@hotmail.com>
2023-06-26 21:21:34 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -106,6 +106,7 @@ def fetch_content(url, numofpages, responseTooLarge, member_id, timeout, summary
 
         
         options = ChromeOptions()
+        options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         options.add_argument("start-maximized")
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
