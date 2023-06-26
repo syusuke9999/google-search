cf73ed756464e4a9eb8d83f9890d2e06a95d65a3
Özgür Özkan <ozgurozkan123@hotmail.com>
2023-06-26 21:25:04 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -105,14 +105,17 @@ def fetch_content(url, numofpages, responseTooLarge, member_id, timeout, summary
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
         
-        options = ChromeOptions()
+          # Use Selenium to fetch conten
+        options = Options()
         options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        options.add_argument("start-maximized")
         options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
-        driver = uc.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
-        
+
+
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
+
         
 
         #driver = uc.Chrome()
