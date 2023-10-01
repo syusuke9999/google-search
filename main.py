801b3db4bba95d5b905bbcd6025f33cbf52db671
syusuke9999 <6879863+syusuke9999@users.noreply.github.com>
2023-10-01 09:42:04 +0900

    Format code according to PEP8 standards

--- main.py
+++ main.py
@@ -8,14 +8,19 @@ import logging
 from urllib.parse import urlparse
 from utils import process_results
 
+logging.basicConfig(level=logging.DEBUG)
+
 app = Flask(__name__)
 CORS(app)
+
 
 def load_environment_variables():
     load_dotenv()
     return os.environ.get("GOOGLE_API_KEY"), os.environ.get("CUSTOM_SEARCH_ENGINE_ID")
 
+
 API_KEY, CX = load_environment_variables()
+
 
 @app.route('/.well-known/ai-plugin.json', methods=['GET'])
 def get_plugin_info():
@@ -26,13 +31,15 @@ def get_plugin_info():
 
         return jsonify(data)
 
+
 def is_url(query):
     try:
         result = urlparse(query)
         return all([result.scheme, result.netloc])
     except ValueError:
         return False
-        
+
+
 @app.route('/search', methods=['GET'])
 def search():
     member_id = request.headers.get('X-PluginLab-User-Id')
@@ -62,33 +69,36 @@ def search():
     if is_url(query):  # if query is a URL
         results = []
         results.append({'link': query, 'title': 'User given url in query'})
-        formatted_results = process_results(results,numofpages,responseTooLarge,member_id)
+        formatted_results = process_results(results, numofpages, responseTooLarge, member_id)
         return jsonify({"results": formatted_results})
     else:
         url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={CX}&q={query}&num={numofpages}"
         response = requests.get(url)
-    
+
         if response.status_code == 200:
             data = response.json()
             results = data.get('items', [])
-            formatted_results = process_results(results,numofpages,responseTooLarge,member_id)
+            formatted_results = process_results(results, numofpages, responseTooLarge, member_id)
             if not formatted_results:  # if formatted_results is empty
                 if is_url(query):  # if query is a URL
                     results.append({'link': query, 'title': 'User given url in query'})
-                    formatted_results = process_results(results,numofpages,responseTooLarge,member_id)
+                    formatted_results = process_results(results, numofpages, responseTooLarge, member_id)
                     return jsonify({"results": formatted_results})
                 else:
-                    return jsonify({"results": "Google search result is empty. There's no such information on the web."})
-        
+                    return jsonify(
+                        {"results": "Google search result is empty. There's no such information on the web."})
+
             return jsonify({"results": formatted_results})
         else:
             error_data = response.json()  # Get JSON data from the error response
             print(f"Google API gave error fetching search results: {error_data}")  # Print the error data
             return jsonify({"error": "Error fetching search results", "details": error_data}), response.status_code
 
+
 @app.route('/.well-known/<path:filename>')
 def serve_well_known_files(filename):
     return send_from_directory(os.path.join(os.getcwd(), ".well-known"), filename)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
